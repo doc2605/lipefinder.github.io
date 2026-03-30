c8a2d1693658921d4fae11d5da35cf882cac2a9f
doc2605 <joaofelipemartines@gmail.com>
2026-03-29 22:00:33 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { defineConfig, loadEnv } from 'vite';
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: '/lipefinder.github.io/', // <--- adiciona o nome do teu repo
+    base: '/lipefinder/', // <--- adiciona o nome do teu repo
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
